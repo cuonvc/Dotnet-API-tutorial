37707cfd3094395209de74aff7dffa4bcbb432c2
cuonvc <cuong.fithau@gmail.com>
2023-04-27 23:57:22 +0700

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data {
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo {
+    public class Student {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
